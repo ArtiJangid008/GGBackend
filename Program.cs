cc8bf6613a0b0012c1b57ff6c25e92912b4f0730
arhanuma <arhanuma@LIN-CYB9KN3>
2025-07-02 14:40:09 +0530

    Fix swagger

--- Program.cs
+++ Program.cs
@@ -15,7 +15,9 @@ builder.Services.AddCors(options =>
 // Add services to the container.
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    ));
 
 
 builder.Services.AddControllers();
